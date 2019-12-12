0f028f86a82ebde9a902a6fe333e3d4f945588cb
Jaron Mink <jaronmm2@illinois.edu>
2019-12-11 18:23:17 -0600

    Fix trojaned TRNG and make it synchronized with a global clock.

--- README.md
+++ README.md
@@ -27,23 +27,7 @@ Results (Final)
 ---------------
   - Pass DIEHARD and other randomness tests while maintaining the hardware trojan
 
-[Base TRNG Implementation stolen from Wold, Tan, and emanuelen5!](https://github.com/emanuelen5/Wold_Tan_TRNG)
+Repository Organization
+-----------------------
 
-The Original Readme 
-==================
-
-# True Random Number Generator by Wold and Tang
-True Random Number Generator for FPGA. Based on article by Wold and Tan [1].
-
-## Navigating the repository
-* `/`  
-  contains the TRNG entity.
-* `/simulation/`  
-  contains a test bench for "stimulating" the TRNG and some .do-files for compiling the simulation files and displaying the waveforms nicely. The .do-macros are run in Modelsim by typing `do <filename.do>`.
-* `/synthesis/`  
-  contains a top for synthesizing the entity in an FPGA.
-
-## Customizing
-The degree of randomness is mainly controlled by the number of oscillator rings that are in parallel. There are other factors as well, which [1] explains very nicely.
-
-[1] [Wold and Tan, "Analysis and Enhancement of Random Number Generator in FPGA Based on Oscillator Rings," International Journal of Reconfigurable Computing, Volume 2009](http://dx.doi.org/10.1155/2009/501672 "DOI link")
+`Trojaned_TRNG.vhd` contains the top-level definition for the trojaned ring oscillator.
--- Trojaned_TRNG.vhd
+++ Trojaned_TRNG.vhd
@@ -2,41 +2,48 @@ library ieee;
   use ieee.std_logic_1164.all;
   use ieee.numeric_std.all;
 
--- A True Random Number Generator based on
--- Wold and Tan, "Analysis and Enhancement of Random Number Generator in FPGA
--- Based on Oscillator Rings," International Journal of Reconfigurable
--- Computing, Volume 2009, doi: http://dx.doi.org/10.1155/2009/501672.
 entity Trojaned_TRNG is
   generic (
-    -- The number of inverters in each oscillator loop. A lower number gives a
-    -- higher average frequency since the signal has to propagate a shorter
-    -- distance for each loop, but lower variance since the inverters are more
-    -- likely to be located in the same slice)
-    G_INVERTER_DEPTH : positive :=  3;
-    -- The number of oscillator loops in parallel (increases randomness)
-    G_N_RINGS        : positive := 1
+    G_N_INVERTERS    : positive :=  3;
+    G_N_RINGS        : positive :=  4
   );
+  
   port (
-    clk_sys  : in  std_logic;
-    rst_n    : in  std_logic;
     bit_out  : out std_logic
   );
-end entity; -- Trojaned_TRNG
+end entity;
 
 architecture rtl of Trojaned_TRNG is
 
   -- Propogation Delay Constants
-  constant C_PROP_DELAY : time := 9.837 ps;
+  constant C_PROP_DELAY : time := 10 ps;
+  
+  signal rst_n : std_logic := '1';
+  signal clk_sys_g : std_logic := '0';
+  signal clk_sys : std_logic := '0';
 
-  type oscillator_ring_ary_t is array (natural range <>) of std_logic_vector(G_INVERTER_DEPTH-1 downto 0);
+  type oscillator_ring_ary_t is array (natural range <>) of std_logic_vector(G_N_INVERTERS-1 downto 0);
   signal oscillator_ring : oscillator_ring_ary_t(G_N_RINGS-1 downto 0);
+  
   attribute keep: boolean;
-  attribute keep of oscillator_ring: signal is true;
+  attribute keep of oscillator_ring: signal is true;  
+--  attribute dont_touch: boolean;
+--  attribute dont_touch of oscillator_ring: signal is true;
 
-  signal ring_reg_out    : std_logic_vector(G_N_RINGS-1 downto 0);
-  signal bit_out_i, xor_sample : std_logic;
+  signal ring_reg_out    : std_logic_vector(G_N_RINGS-1 downto 0) := "0000";
+  signal bit_out_i : std_logic;
 begin
-  assert (G_INVERTER_DEPTH mod 2 = 1) report "There must be an odd number of inverters in the oscillator rings to become astable." severity failure;
+  clk_sys_g <= not clk_sys_g after (C_PROP_DELAY / 2);
+  
+  proc_clock_tick : process
+  begin
+    wait until clk_sys_g = '1';
+    clk_sys <= not clk_sys;
+    wait until clk_sys_g = '0';
+    wait until clk_sys_g = '1';
+    wait until clk_sys_g = '0';
+    wait until clk_sys_g = '1';
+  end process;
 
   -- Mapping internal representations of top signals
   bit_out <= bit_out_i;
@@ -45,45 +52,27 @@ begin
   gen_oscillator_rings : for i in 0 to G_N_RINGS-1 generate
 
     -- Connect the oscillator rings in a loop (moving downward). The output (bottom) is also connected to the reset.
-    oscillator_ring(i)(0) <= not oscillator_ring(i)(1) when rst_n = '1' else '0' after (C_PROP_DELAY);
+    oscillator_ring(i)(0) <= not ring_reg_out(i) after (C_PROP_DELAY / (G_N_INVERTERS + 2)) * 1;
+    oscillator_ring(i)(1) <= not oscillator_ring(i)(0) after (C_PROP_DELAY / (G_N_INVERTERS + 2)) * 2;
+    oscillator_ring(i)(2) <= not oscillator_ring(i)(1) after (C_PROP_DELAY / (G_N_INVERTERS + 2)) * 3;
+    ring_reg_out(i) <= oscillator_ring(i)(2) after (C_PROP_DELAY / (G_N_INVERTERS + 2)) * 4;
 
-    gen_oscillator_ring : for j in 1 to G_INVERTER_DEPTH-1 generate
-      oscillator_ring(i)(j) <= not oscillator_ring(i)((j+1) mod G_INVERTER_DEPTH) after (C_PROP_DELAY);
-    end generate; -- gen_oscillator_ring
 
   end generate; -- gen_oscillator_rings
 
-  test_proc : process(rst_n)
-  begin
-    if rising_edge(rst_n) then
-      test_loop : for i in 1 to G_INVERTER_DEPTH-1 loop
-        report "Connecting oscillator [" & integer'image(i) & " <= " & integer'image((i+1) mod G_INVERTER_DEPTH) & "]" severity note;
-      end loop ; -- test_loop
-    end if;
-  end process ; -- test_proc
-
   -- Sample all of the rings and XOR the result
   proc_sample_rings : process(clk_sys, rst_n)
-    variable xor_result_v : std_logic;
+    variable xor_result_v0 : std_logic := '0';
+    variable xor_result_v1 : std_logic := '0';
   begin
     if (rst_n = '0') then
       bit_out_i  <= '0';
-      xor_sample <= '0';
-      ring_reg_out <= (others => '0');
     elsif rising_edge(clk_sys) then
 
-      -- Sample the oscillator rings
-      sample_oscillator_rings : for i in 0 to G_N_RINGS-1 loop
-        ring_reg_out(i) <= oscillator_ring(i)(0);
-      end loop ; -- sample_oscillator_rings
-
       -- XOR the registered outputs of the oscillator rings
-      xor_result_v := '0';
-      xor_oscillator_rings : for i in 0 to G_N_RINGS-1 loop
-        xor_result_v := xor_result_v xor ring_reg_out(i);
-      end loop ; -- xor_oscillator_rings
-      xor_sample <= xor_result_v;
-      bit_out_i <= xor_sample; -- Double register the sampled value
+      xor_result_v0 := ring_reg_out(0) xor ring_reg_out(1);
+      xor_result_v1 := ring_reg_out(2) xor ring_reg_out(3);
+      bit_out_i <= xor_result_v0 xor xor_result_v1; -- Double register the sampled value
     end if;
   end process; -- proc_sample_rings
 
