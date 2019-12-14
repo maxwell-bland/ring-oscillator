9728690969ebad12422a84c7b624db921734bd20
Jaron Mink <jaronmm2@illinois.edu>
2019-12-14 00:14:00 -0600

    Kinda working

--- TRNG_IO.vhd
+++ TRNG_IO.vhd
@@ -43,7 +43,6 @@ entity TRNG_IO is
   );
   
   Port ( 
-    usb_rx : out std_logic;
 --    gpio_sw_c : in std_logic;
     sysclk_p : in std_logic;
     sysclk_n : in std_logic
@@ -53,6 +52,7 @@ end TRNG_IO;
 architecture rtl of TRNG_IO is
   signal sysclk_ds : std_logic := '0';
   signal sysclk : std_logic := '0';
+  signal rst : std_logic := '0';
   signal w_ready : std_logic;
   signal transmit : std_logic := '0';
   signal transmit_active : std_logic;
@@ -62,74 +62,86 @@ architecture rtl of TRNG_IO is
   signal place : integer := 0;
   signal insertNewLine : std_logic := '0';
   
-    function Bin2Hexascii (
-    signal binary : std_logic_vector(3 downto 0))
-    return std_logic_vector is
-    variable v_temp : std_logic_vector(7 downto 0);
-  begin
-    v_temp := std_logic_vector((unsigned(X"0" & binary) + 65));
-    return v_temp;
+  COMPONENT ila_results PORT (
+    clk    : in  STD_ULOGIC;
+    probe0 : in   STD_ULOGIC_VECTOR(G_SZ_FREQ - 1 downto 0);
+    probe1 : in   STD_ULOGIC );
+  END COMPONENT;
+--    function Bin2Hexascii (
+--    signal binary : std_logic_vector(3 downto 0))
+--    return std_logic_vector is
+--    variable v_temp : std_logic_vector(7 downto 0);
+--  begin
+--    v_temp := std_logic_vector((unsigned(X"0" & binary) + 65));
+--    return v_temp;
     
-  end Bin2Hexascii;
+--  end Bin2Hexascii;
   
 begin
-  IBUFDS_inst : IBUFDS
-  generic map (
-    DIFF_TERM => FALSE,
-    IBUF_LOW_PWR => TRUE, 
-    IOSTANDARD => "DEFAULT")
-  port map (
-    O => sysclk_ds, 
-    I => sysclk_p, 
-    IB => sysclk_n 
-  );
+  sysclk <= not sysclk after 10 ns;
+--  IBUFDS_inst : IBUFDS
+--  generic map (
+--    DIFF_TERM => FALSE,
+--    IBUF_LOW_PWR => TRUE, 
+--    IOSTANDARD => "DEFAULT")
+--  port map (
+--    O => sysclk_ds, 
+--    I => sysclk_p, 
+--    IB => sysclk_n 
+--  );
   
-  BUFG_inst : BUFG
-  port map (
-     O => sysclk,
-     I => sysclk_ds 
-  );
+--  BUFG_inst : BUFG
+--  port map (
+--     O => sysclk,
+--     I => sysclk_ds 
+--  );
   
   TRNG_Controller : entity work.TRNG_Controller(rtl) port map (
-    gpio_sw_c => '0',
+    rst => rst,
     sysclk => sysclk,
     freq_out => out_cnt,
     write_enb => w_ready
   );
+  
+--  ila : ila_results port map (
+--    clk => std_ulogic(sysclk),
+--    probe0 => std_ulogic_vector(out_cnt),
+--    probe1 => std_ulogic(w_ready)
+--  );
 
-  UART_Transmit : entity work.UART_TX(rtl) port map (
-    i_Clk       =>  sysclk,
-    i_TX_DV     =>  transmit,
-    i_TX_Byte   =>  transmit_byte,
-    o_TX_Active =>  transmit_active,
-    o_TX_Serial =>  usb_rx,
-    o_TX_Done   =>  open
-  );
+--  UART_Transmit : entity work.UART_TX(rtl) port map (
+--    i_Clk       =>  sysclk,
+--    i_TX_DV     =>  transmit,
+--    i_TX_Byte   =>  transmit_byte,
+--    o_TX_Active =>  transmit_active,
+--    o_TX_Serial =>  usb_rx,
+--    o_TX_Done   =>  open
+--  );
 
 
-  process 
-  begin
-    if w_ready = '1' and transmit_active = '0' then
-      transmit <= '1';
+--  process 
+--  begin
+--    if w_ready = '1' and transmit_active = '0' then
+--      transmit <= '1';
 
-      if insertNewLine = '1' then
-        insertNewLine <= '0';
-        transmit_byte <= X"0A";  --X"A" is \n in ascii
-      else
-        out_working <= std_logic_vector(shift_right(unsigned(out_cnt), place * 4)) AND X"000000000000000F";
---        out_working <= X"000000000000000A";
-        transmit_byte <=  Bin2Hexascii(out_working(3 downto 0));
+--      if insertNewLine = '1' then
+--        insertNewLine <= '0';
+--        transmit_byte <= X"0A";  --X"A" is \n in ascii
+--      else
+--        out_working <= std_logic_vector(shift_right(unsigned(out_cnt), place * 4)) AND X"000000000000000F";
+----        out_working <= X"000000000000000A";
+--        transmit_byte <=  Bin2Hexascii(out_working(3 downto 0));
       
-        if place = 15 then
-          insertNewLine <= '1';
-          place <= 0;
-        else 
-          place <= place + 1;
-        end if;
-      end if;
+--        if place = 15 then
+--          insertNewLine <= '1';
+--          place <= 0;
+--        else 
+--          place <= place + 1;
+--        end if;
+--      end if;
       
-     else
-      transmit <= '0';
-    end if;
-  end process;
+--     else
+--      transmit <= '0';
+--    end if;
+--  end process;
 end rtl;
--- Trojaned_TRNG.vhd
+++ Trojaned_TRNG.vhd
@@ -11,46 +11,50 @@ entity Trojaned_TRNG is
   );
   
   port (
-    rst_n : in std_logic;
+    sysclk : in std_logic;
+    rst : in std_logic;
     counter_out : out std_logic_vector(G_N_TFF - 1 downto 0)
   );
 end entity;
 
 architecture rtl of Trojaned_TRNG is
+ 
 
   type oscillator_ring_ary_t is array (natural range <>) of std_logic_vector(G_N_INVERTERS-1 downto 0);
-  signal oscillator_ring : oscillator_ring_ary_t(G_N_RINGS-1 downto 0);
+  signal oscillator_ring : oscillator_ring_ary_t(G_N_RINGS-1 downto 0) := ( 0 => "000" );
   
-  attribute keep: boolean;
-  attribute keep of oscillator_ring: signal is true;  
+  
+--  attribute keep: boolean;
+--  attribute keep of oscillator_ring: signal is true;  
   attribute dont_touch: boolean;
   attribute dont_touch of oscillator_ring: signal is true;
 
   signal ring_reg_out    : std_logic_vector(G_N_RINGS-1 downto 0) := "0";
-  signal bit_out_i : std_logic;
-  signal intermediate : std_logic_vector(G_N_TFF - 1 downto 0);
+  signal intermediate : std_logic_vector(G_N_TFF - 1 downto 0) := X"0000000000000000";
 begin
 
   -- Generate oscillator rings in parallel
-  gen_oscillator_rings : for i in 0 to G_N_RINGS-1 generate
-    -- Connect the oscillator rings in a loop (moving downward). The output (bottom) is also connected to the reset.
-    oscillator_ring(i)(0) <= (not oscillator_ring(i)(2)) and (not rst_n);
-    oscillator_ring(i)(1) <= not oscillator_ring(i)(0); 
-    oscillator_ring(i)(2) <= not oscillator_ring(i)(1); 
-  end generate;
+--  gen_oscillator_rings : for i in 0 to G_N_RINGS-1 generate
+--    -- Connect the oscillator rings in a loop (moving downward). The output (bottom) is also connected to the reset.
+--    oscillator_ring(i)(0) <= not oscillator_ring(i)(2);
+--    oscillator_ring(i)(1) <= not oscillator_ring(i)(0); 
+--    oscillator_ring(i)(2) <= not oscillator_ring(i)(1); 
+--  end generate;
+  
+  
 
   TFF_0 : entity work.TFlipFlop(rtl) port map (
-           CLK => not oscillator_ring(0)(2),
-           RESET => rst_n,
+           CLK => sysclk,--not oscillator_ring(0)(2),
+           RESET => rst,
            T => '1',
            Q => intermediate(0),
-           CLK_enable => not oscillator_ring(0)(2) 
+           CLK_enable => sysclk
   );  
   
    gen_TFF : for i in 1 to G_N_TFF - 1 generate
            TFF_i : entity work.TFlipFlop(rtl) port map (
            CLK => not intermediate(i-1),
-           RESET => rst_n,
+           RESET => rst,
            T => '1',
            Q => intermediate(i),
            CLK_enable => not intermediate(i-1) 
